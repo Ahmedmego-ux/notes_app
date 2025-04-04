aa354cdd486155ec4527172850bddb18cc24f788
ahmed <ahmedmagdy0102926@gmail.com>
2025-04-04 02:56:11 +0200

    add field to note bottom

--- lib/views/widgets/add_note_bottom.dart
+++ lib/views/widgets/add_note_bottom.dart
@@ -3,12 +3,17 @@ import 'package:flutter_application_5/views/widgets/custome_text_field.dart';
 
 class Addnotebottomshet extends StatelessWidget{
     Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
           SizedBox(height: 30,),
-          CustomeTextField()
+          CustomeTextField(
+            hintText: 'tittle',
+          ),
+          SizedBox(height: 15,),
+          CustomeTextField(hintText: 'content',
+          maxlines: 5,)
         ],
       ),
     );
--- lib/views/widgets/custome_text_field.dart
+++ lib/views/widgets/custome_text_field.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_5/constant.dart';
 
 class CustomeTextField extends StatelessWidget{
+  final String hintText;
+  final int maxlines;
+
+  const CustomeTextField({super.key, required this.hintText,  this.maxlines=1});
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxlines,
       cursorColor: kprimarycolor,
       decoration: InputDecoration(
-        hintText: 'tittle',
+        hintText: hintText,
         hintStyle: TextStyle(color: kprimarycolor),
         border: buildborder(),
         enabledBorder: buildborder(),
